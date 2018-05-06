c421359d3256db2aac1ceca73772463b66ee8138
kenchang111 <floodtide.ego3.na@gmail.com>
2018-05-06 07:55:43 +0000

    Implement basic login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      # エラーメッセージを作成する
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+end
